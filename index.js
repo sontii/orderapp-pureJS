ea705ba0f669c5135cfb3a7cafe650bca7a037e1
sontii <sontii78@gmail.com>
2022-12-04 00:03:26 +0100

    order

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
 import { foodData } from "./food.js"
 
+let orderArr = []
 
+document.body.addEventListener('click', function(e){
+    if(e.target.dataset.type) {
+        createOrder(e.target.dataset.type)
+    }
+    else if (e.target.dataset.remove){
+        removeOrder(e.target.dataset.remove);
+    }
+})
+
+function createOrder(foodType) {
+    orderArr.push(foodData.filter((food) => food.type === foodType)); 
+}
+
+function removeOrder(foodType) {
+	orderArr.push(foodData.filter((food) => food.type === foodType));
+}
+
+function renderOrder(){
+    const orderItems = document.getElementById("order-items");
+		let items = "";
+
+		orderArr.forEach(function(food) {
+			items += `
+        <div>
+            <span class="food food-type">${food.type}</span>
+            <span class="food food-toppings" data-remove="${food.type}">remove</span>
+            <span class="food food-price">$${food.price}</span>
+        </div>
+        `;
+		});
+
+    orderItems.innerHTML = items 
+}
 
 function render(){
-    const foodContainer = document.getElementById('food-container')
+    const foodWrapper = document.getElementById('food-wrapper')
     let foodHtml = ''
 
     foodData.forEach(function(food){
         foodHtml += `
-            <div>
-                <img class="food-img" src="${food.image}" alt="food pic">
+            <div class="food-container" id=food-container>
+                <div class="food-img-wrap">
+                    <img class="food-img" src="${food.image}" alt="food pic">
+                </div>    
                 <div class="food-title">
-                    <span>
-                        <p>${food.type}<p>
-                        <p>${food.toppings}<p>
-                        <p>$${food.price}<p>
-                    </span>
+                        <div class="food food-type">${food.type}</div>
+                        <div class="food food-toppings">${food.toppings}</div>
+                        <div class="food food-price">$${food.price}</div>  
                 </div>
-                <div class="food-add">
+                <div class="food-add" data-type="${food.type}">
                     +
                 </div>
             </div>
@@ -26,7 +60,7 @@ function render(){
 
     })
 
-    foodContainer.innerHTML = foodHtml
+    foodWrapper.innerHTML = foodHtml;
 
 }
 
